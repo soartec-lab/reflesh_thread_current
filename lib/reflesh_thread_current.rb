6f062c8a6b110fe3413d4f2af642f34089fdfca6
soartec-lab <info@soartec-lab.work>
2020-08-28 01:07:08 +0900

    bundle gem reflesh_thread_current -t

---
+++ lib/reflesh_thread_current.rb
@@ -0,0 +1,6 @@
+require "reflesh_thread_current/version"
+
+module RefleshThreadCurrent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reflesh_thread_current/version.rb
@@ -0,0 +1,3 @@
+module RefleshThreadCurrent
+  VERSION = "0.1.0"
+end
