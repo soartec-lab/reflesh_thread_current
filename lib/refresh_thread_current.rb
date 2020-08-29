7e358d5b333d3ce766215c59bcca957b6c9956f1
soartec-lab <info@soartec-lab.work>
2020-08-29 13:07:58 +0900

    Defined amethods to delete thread local variables

--- lib/refresh_thread_current.rb
+++ lib/refresh_thread_current.rb
@@ -1,6 +1,22 @@
 require "refresh_thread_current/version"
 
 module RefreshThreadCurrent
-  class Error < StandardError; end
-  # Your code goes here...
+  autoload :Middleware, 'refresh_thread_current/middleware'
+
+  module_function
+
+  def clear_all_local_variables
+    clear_thread_local_variables
+    clear_fiber_local_variables
+  end
+
+  def clear_thread_local_variables
+    Thread.current.thread_variables.each do |key|
+      Thread.current.thread_variable_set(key, nil)
+    end
+  end
+
+  def clear_fiber_local_variables
+    Thread.current.keys.each { |key| Thread.current[key] = nil }
+  end
 end
--- spec/refresh_thread_current_spec.rb
+++ spec/refresh_thread_current_spec.rb
@@ -3,7 +3,60 @@ RSpec.describe RefreshThreadCurrent do
     expect(RefreshThreadCurrent::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".clear_all_local_variables" do
+    context "thread local vriable and fiber local variable is stored" do
+      before do
+        Thread.current.thread_variable_set(:foo, 1)
+        Thread.current[:bar] = 2
+      end
+
+      it "all local variables be nil" do
+        expect(Thread.current.thread_variable_get(:foo)).to eq(1)
+        expect(Thread.current[:bar]).to eq(2)
+
+        RefreshThreadCurrent.clear_all_local_variables
+
+        expect(Thread.current.thread_variable_get(:foo)).to eq(nil)
+        expect(Thread.current[:bar]).to eq(nil)
+      end
+    end
+  end
+
+  describe ".clear_thread_local_variables" do
+    context "thread local vriables is stored" do
+      before do
+        Thread.current.thread_variable_set(:foo, 1)
+        Thread.current.thread_variable_set(:bar, 2)
+      end
+
+      it "all local variables be nil" do
+        expect(Thread.current.thread_variable_get(:foo)).to eq(1)
+        expect(Thread.current.thread_variable_get(:bar)).to eq(2)
+
+        RefreshThreadCurrent.clear_thread_local_variables
+
+        expect(Thread.current.thread_variable_get(:foo)).to eq(nil)
+        expect(Thread.current.thread_variable_get(:bar)).to eq(nil)
+      end
+    end
+  end
+
+  describe ".clear_fiber_local_variables" do
+    context "fiber local vriables is stored" do
+      before do
+        Thread.current[:foo] = 1
+        Thread.current[:bar] = 2
+      end
+
+      it "all local variables be nil" do
+        expect(Thread.current[:foo]).to eq(1)
+        expect(Thread.current[:bar]).to eq(2)
+
+        RefreshThreadCurrent.clear_fiber_local_variables
+
+        expect(Thread.current[:foo]).to eq(nil)
+        expect(Thread.current[:bar]).to eq(nil)
+      end
+    end
   end
 end
